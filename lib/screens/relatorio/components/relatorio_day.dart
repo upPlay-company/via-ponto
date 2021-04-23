71af13705a3bcf62084a76c1b49be0063ecc6e91
Alexandrebmx5 <alexandrebmx5@gmail.com>
2021-04-23 17:37:58 -0400

    commit - relatorio day - bater ponto - Alexandre

--- lib/screens/relatorio/components/relatorio_day.dart
+++ lib/screens/relatorio/components/relatorio_day.dart
@@ -1,80 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:viaponto_oficial/screens/home/components/active_ponto_day.dart';
+import 'package:viaponto_oficial/model/bater_ponto/bater_ponto.dart';
 import 'package:viaponto_oficial/store/myponto_store.dart';
 
-class RelatorioDay extends StatefulWidget {
-  @override
-  _RelatorioDayState createState() => _RelatorioDayState();
-}
+class RelatorioDay extends StatelessWidget {
+  RelatorioDay(this.ponto, this.store);
 
-class _RelatorioDayState extends State<RelatorioDay> {
-  final MyPontoStore store = MyPontoStore();
+  final BaterPonto ponto;
+  final MyPontoStore store;
+
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverToBoxAdapter(
-            child: Column(
+      return Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              color: Theme.of(context).primaryColor,
-              height: 70,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 50, right: 50),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    GestureDetector(
-                        onTap: () {}, child: Icon(Icons.arrow_back_ios)),
-                    Text(
-                      'dias',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    GestureDetector(
-                        onTap: () {}, child: Icon(Icons.arrow_forward_ios))
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              color: Colors.grey[200],
-              height: 80,
+            Padding(
+              padding: const EdgeInsets.all(16),
               child: Row(
                 children: [
                   Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                    child: Row(
                       children: [
-                        Text('HORAS TOTAIS'),
-                        SizedBox(height: 10),
-                        Text('08:00')
+                        Text(ponto.time,
+                            style:
+                            TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Text(
+                          '${ponto.registro}',
+                          style: TextStyle(fontSize: 18),
+                        ),
                       ],
                     ),
                   ),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                  Padding(
+                    padding: const EdgeInsets.only(right: 15),
+                    child: Row(
                       children: [
-                        Text('HORAS EXTRAS'),
-                        SizedBox(height: 10),
-                        Text('01:00')
+                        GestureDetector(
+                          onTap: (){},
+                          child: Icon(Icons.description)),
                       ],
                     ),
-                  ),
+                  )
                 ],
               ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(color: Colors.black.withAlpha(50)))),
             )
           ],
-        )),
-        SliverToBoxAdapter(
-          child: ListView.builder(
-            itemCount: store.allAds.length,
-            itemBuilder: (_, index) {
-              return ActivePontoDay(store.allAds[index], store);
-            },
-          ),
-        )
-      ],
-    );
+        ),
+      );
   }
 }
--- lib/screens/relatorio/relatorio_screen.dart
+++ lib/screens/relatorio/relatorio_screen.dart
@@ -1,13 +1,14 @@
 import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:viaponto_oficial/screens/relatorio/components/relatorio_month.dart';
 import 'package:viaponto_oficial/screens/relatorio/components/relatorio_week.dart';
+import 'package:viaponto_oficial/store/myponto_store.dart';
 
 import 'components/relatorio_day.dart';
 
 class RelatorioScreen extends StatefulWidget {
-
   RelatorioScreen({this.inicialPage = 0});
 
   final int inicialPage;
@@ -16,7 +17,9 @@ class RelatorioScreen extends StatefulWidget {
   _RelatorioScreenState createState() => _RelatorioScreenState();
 }
 
-class _RelatorioScreenState extends State<RelatorioScreen> with SingleTickerProviderStateMixin {
+class _RelatorioScreenState extends State<RelatorioScreen>
+    with SingleTickerProviderStateMixin {
+  final MyPontoStore store = MyPontoStore();
 
   TabController tabController;
 
@@ -32,7 +35,10 @@ class _RelatorioScreenState extends State<RelatorioScreen> with SingleTickerProv
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Relatório', style: TextStyle(color: Colors.white),),
+        title: Text(
+          'Relatório',
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
         iconTheme: IconThemeData(color: Colors.white),
         automaticallyImplyLeading: false,
@@ -57,8 +63,77 @@ class _RelatorioScreenState extends State<RelatorioScreen> with SingleTickerProv
       body: TabBarView(
         controller: tabController,
         children: [
-          Container(
-            child: RelatorioDay(),
+          CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
+                  child: Column(
+                children: [
+                  Container(
+                    color: Theme.of(context).primaryColor,
+                    height: 70,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 50, right: 50),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          GestureDetector(
+                              onTap: () {}, child: Icon(Icons.arrow_back_ios)),
+                          Text(
+                            'dias',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          GestureDetector(
+                              onTap: () {},
+                              child: Icon(Icons.arrow_forward_ios))
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.grey[200],
+                    height: 80,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('HORAS TOTAIS'),
+                              SizedBox(height: 10),
+                              Text('08:00')
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('HORAS EXTRAS'),
+                              SizedBox(height: 10),
+                              Text('01:00')
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )),
+              Observer(
+                builder: (_) {
+                  if (store.allAds.isEmpty) return Container();
+
+                  final List<Widget> children =
+                      store.allAds.map<Widget>((section) {
+                    return RelatorioDay(section, store);
+                  }).toList();
+
+                  return SliverList(
+                    delegate: SliverChildListDelegate(children),
+                  );
+                },
+              )
+            ],
           ),
           Container(
             child: RelatorioWeek(),
