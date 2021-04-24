574f623e049618685a7943f38cadbad63eb94d3f
mason <paulodanielaraujo.pd90@gmail.com>
2021-04-24 13:05:39 -0400

    13:05 24/04/2021 => DaysFloatReplay.dart

--- lib/screens/relatorio/components/relatorio_day_screen.dart
+++ lib/screens/relatorio/components/relatorio_day_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:viaponto_oficial/screens/relatorio/components/daysfloatreplay.dart';
 import 'package:viaponto_oficial/screens/relatorio/components/relatorio_day.dart';
 import 'package:viaponto_oficial/store/myponto_store.dart';
 
 class RelatorioDayScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     final MyPontoStore store = MyPontoStore();
 
     return Scaffold(
@@ -14,64 +14,50 @@ class RelatorioDayScreen extends StatelessWidget {
         slivers: [
           SliverToBoxAdapter(
               child: Column(
-                children: [
-                  Container(
-                    color: Theme.of(context).primaryColor,
-                    height: 70,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 50, right: 50),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                color: Theme.of(context).primaryColor,
+                height: 70,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 50, right: 50),
+                  child: DaysFloatReplay(),
+                ),
+              ),
+              Container(
+                color: Colors.grey[200],
+                height: 80,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          GestureDetector(
-                              onTap: () {}, child: Icon(Icons.arrow_back_ios)),
-                          Text(
-                            'dias',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          GestureDetector(
-                              onTap: () {},
-                              child: Icon(Icons.arrow_forward_ios))
+                          Text('HORAS TOTAIS'),
+                          SizedBox(height: 10),
+                          Text('08:00')
                         ],
                       ),
                     ),
-                  ),
-                  Container(
-                    color: Colors.grey[200],
-                    height: 80,
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text('HORAS TOTAIS'),
-                              SizedBox(height: 10),
-                              Text('08:00')
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text('HORAS EXTRAS'),
-                              SizedBox(height: 10),
-                              Text('01:00')
-                            ],
-                          ),
-                        ),
-                      ],
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('HORAS EXTRAS'),
+                          SizedBox(height: 10),
+                          Text('01:00')
+                        ],
+                      ),
                     ),
-                  )
-                ],
-              )),
+                  ],
+                ),
+              )
+            ],
+          )),
           Observer(
             builder: (_) {
               if (store.allAds.isEmpty) Container();
 
-              final List<Widget> children =
-              store.allAds.map<Widget>((section) {
+              final List<Widget> children = store.allAds.map<Widget>((section) {
                 return RelatorioDay(section, store);
               }).toList();
 
